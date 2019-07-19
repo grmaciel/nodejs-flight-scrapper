74bc84ab36fff376d3ba598f0f60109fbdd67c73
Gilson Maciel <gilson.maciel@mercedes-benz.io>
2019-07-19 22:47:07 +0200

    added support to add origin

--- destiny.js
+++ destiny.js
@@ -1,14 +1,24 @@
 const destinySelector = '#flt-app > div.gws-flights__flex-column.gws-flights__flex-grow > main.gws-flights__flex-column.gws-flights__active-tab.gws-flights__home-page > div:nth-child(3) > div > div.gws-flights-form__form-card > div > div.gws-flights__flex-box.gws-flights__align-center > div.flt-input.gws-flights-form__input-container.gws-flights__flex-box.gws-flights-form__airport-input.gws-flights-form__empty.gws-flights-form__swapper-left';
+const originSelector = '#flt-app > div.gws-flights__flex-column.gws-flights__flex-grow > main.gws-flights__flex-column.gws-flights__active-tab.gws-flights__home-page > div:nth-child(3) > div > div.gws-flights-form__form-card > div > div.gws-flights__flex-box.gws-flights__align-center > div.flt-input.gws-flights-form__input-container.gws-flights__flex-box.gws-flights-form__airport-input.gws-flights-form__swapper-right > span.gws-flights-form__location-icon.gws-flights-form__origin-icon > span > svg';
+const fromToInputSelector = '#sb_ifc50 > input[type=text]'
 
-async function fillDestiny(page, destiny) {
-    console.log('Filling your destiny...')
-    await page.focus(destinySelector);
-    await page.keyboard.type(destiny)
-    await page.screenshot({ path: 'screenshots/destiny.png' });
-    await page.keyboard.press('Enter');
-    await page.screenshot({ path: 'screenshots/destiny_filled.png' });
+async function fillOriginDestiny(page, origin, destiny) {
+    console.log(`Filling flight from ${origin} to ${destiny}`)
+
+    await fillInputField(page, origin, originSelector)
+    await fillInputField(page, destiny, destinySelector)
+}
+
+// I've no idea but i can only input those fucking fields
+// if i take a screenshot, i'm open to suggestions
+async function fillInputField(page, text, fieldSelector) {
+    await page.tap(fieldSelector)
+    await page.focus(fromToInputSelector)
+    await page.keyboard.type(text)
+    await page.screenshot({ path: 'screenshots/input.png' });
+    await page.keyboard.press('Tab');
 }
 
 module.exports = {
-	fillDestiny: fillDestiny
+    fillOriginDestiny: fillOriginDestiny
 };
