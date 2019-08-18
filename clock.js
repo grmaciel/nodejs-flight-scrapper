3c1b092b846ecfdd0ddce91d347734e7ae10cbde
Silvio Fragnani <silviofragnanisilva@gmail.com>
2019-08-18 19:54:07 +0200

    Change schedule to every 30 min

--- clock.js
+++ clock.js
@@ -3,7 +3,7 @@ const Scrapper = require('./scrapper');
 
 // runs every 5 minutes
 // schedule.scheduleJob('* 8,12,17,22 * * *', function(){
-schedule.scheduleJob('*/4 * * * *', function(){
+schedule.scheduleJob('*/30 * * * *', function(){
     console.log('Scrapper running');
 
     const scrapper = new Scrapper();
