13a5a5cb8361b1474c07f3ab508f8e31550a7fa6
Silvio Fragnani <silviofragnanisilva@gmail.com>
2019-08-19 20:54:47 +0200

    Added better comments and create env to set crontab

--- clock.js
+++ clock.js
@@ -1,10 +1,15 @@
 const schedule = require('node-schedule');
 const Scrapper = require('./scrapper');
 
-// runs every 5 minutes
-// schedule.scheduleJob('* 8,12,17,22 * * *', function(){
-schedule.scheduleJob('*/30 * * * *', function(){
-    console.log('Scrapper running');
+// Example of crontab which probably we'll use later '* 8,12,17,22 * * *', it will run at 8h, 12, 17h and 22h
+var crontab = process.env.CRONTAB
+if (crontab == null) {
+    // define default schedule to every 30 min
+    crontab = '*/30 * * * *'
+}
+
+schedule.scheduleJob(crontab, function(){
+    console.log('Scrapper running ', Date().toString());
 
     const scrapper = new Scrapper();
     scrapper.start();
